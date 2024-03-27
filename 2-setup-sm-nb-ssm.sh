36fa7479223dd025fa71e287dc1a71b94a88adef
Bin Chen <fengzhishuiwo@163.com>
2024-03-28 01:23:38 +0800

    Update 2-setup-sm-nb-ssm.sh

--- 2-setup-sm-nb-ssm.sh
+++ 2-setup-sm-nb-ssm.sh
@@ -8,10 +8,10 @@ set -e
 
 CURRENT_REGION=$(aws configure get region || echo "$AWS_REGION")
 
-if [ ! -f /tmp/ssm/ssm-setup-cli ]; then
-    mkdir -p /tmp/ssm/
+if [! -f /tmp/ssm/ssm-setup-cli]; then
     curl https://amazon-ssm-$CURRENT_REGION.s3.$CURRENT_REGION.amazonaws.com/latest/linux_amd64/ssm-setup-cli -o /tmp/ssm/ssm-setup-cli
 fi
+
 sudo chmod +x /tmp/ssm/ssm-setup-cli
 
 SSH_SSM_ROLE='service-role/AmazonEC2RunCommandRoleForManagedInstances'
@@ -28,13 +28,16 @@ fi
 
 echo "setup-sm-nb-ssm: Detected SageMaker resource: $RESOURCE_NAME [$RESOURCE_ARN]"
 
+SSH_SSM_TAGS="[{\"Key\": \"SSHTimestamp\", \"Value\": \"$SSH_TIMESTAMP\"}, {\"Key\": \"SSHResourceName\", \"Value\": \"$RESOURCE_NAME\"}, {\"Key\": \"SSHResourceArn\", \"Value\": \"$RESOURCE_ARN\"}]"
+
 response=$(aws ssm create-activation \
   --description "Activation for Amazon SageMaker Notebook Instance" \
   --iam-role "$SSH_SSM_ROLE" \
   --registration-limit 1 \
-  --region "$CURRENT_REGION")
+  --region "$CURRENT_REGION" \
+  --tags "$SSH_SSM_TAGS")
 
 acode=$(echo $response | jq --raw-output '.ActivationCode')
 aid=$(echo $response | jq --raw-output '.ActivationId')
 
-sudo /tmp/ssm/ssm-setup-cli -register -activation-id "$aid" -activation-code "$acode" -region "$CURRENT_REGION" -override
+sudo /tmp/ssm/ssm-setup-cli -register -activation-id "$aid" -activation-code "$acode" -region "$CURRENT_REGION" -override -tags "$SSH_SSM_TAGS"
